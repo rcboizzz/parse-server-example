9eaff06e0385e82aa4535742e4b971a1d80cbeb0
rcboizzz <rcboizzz@gmail.com>
2017-01-31 18:03:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ if (!databaseUri) {
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://heroku_sh74n454:9i9vu0u2ue6nm96e2p703mpl7j@ds131139.mlab.com:31139/heroku_sh74n454/dev',
+  databaseURI: databaseUri || 'mongodb://ariel:ariel@ds131099.mlab.com:31099/heroku_flftsq44',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'vApkJIra0Wo89tGnfnq34s2OQxc0F0jEiMi0VQxT',
-  masterKey: process.env.MASTER_KEY || 'brNuan5ygSNMH94copNkmfqWGW6l8rv7s1ZdKEnj', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'denrAppId',
+  masterKey: process.env.MASTER_KEY || 'denrMasterKey', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'https://erdbftrrctally.herokuapp.com/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+    classNames: ["_User", "Client", "Todo"] // List of classes to support for query subscriptions
   }
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
